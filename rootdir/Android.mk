2833aeca2b4af12f8cbe62d9340bce1fc5a661c3
cholokei <leesl0416@naver.com>
2013-08-13 01:59:45 +0900

    Fix typo merge error msm8660: Add build option - NO_COMMON_INIT_QCOM_RC 5c7c8494269eb9287c359ae27672ed71b8ca1f62

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifneq ($(NO_COMMON_INIT_QCOM_RC),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
@@ -10,6 +9,7 @@ LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+ifneq ($(NO_COMMON_INIT_QCOM_RC),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
