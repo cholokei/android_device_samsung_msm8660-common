b4f2d2b3c13ba384af82d995d75f0750dff3f543
cholokei <leesl0416@naver.com>
2014-03-16 12:32:24 +0900

    msm8660: Add build option not to include common fstab.qcom script
    Signed-off-by: cholokei <leesl0416@naver.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifneq ($(NO_COMMON_FSTAB_QCOM),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
@@ -8,6 +9,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
 
 ifneq ($(NO_COMMON_INIT_QCOM_RC),true)
 include $(CLEAR_VARS)
